d873b8687f4f0b892326b56bd1ca3b819bc7a9e0
alejandropascuale <apascuale@outlook.com>
2022-01-28 13:31:11 -0300

    delete useless files and yarn-erro to git ignore

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import Header from './components/Header';
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      'Dosis',
-    ].join(','),
-  },});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Header />
-    </ThemeProvider>
+    </>
   );
 }
 
